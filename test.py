3d5cbafc0d4581c14e78e8aab46e6ae74083c648
MaximZolin67 <maxzol2009@gmail.com>
2025-11-06 20:01:08 +0300

    add google drive

--- test.py
+++ test.py
@@ -1,3 +1,23 @@
-from db import init_db
+# import sqlite3 
+# DB_PATH = "shop.db"
+# conn = sqlite3.connect(DB_PATH)
+# c = conn.cursor()
+# c.execute("UPDATE users SET role = 'admin'")
+# conn.commit()
+# conn.close()
+from pydrive2.auth import GoogleAuth
+from pydrive2.drive import GoogleDrive
 
-init_db()
+gauth = GoogleAuth()
+gauth.LoadCredentialsFile("token.json")
+
+if gauth.access_token_expired:
+    gauth.Refresh()
+gauth.SaveCredentialsFile("token.json")
+
+drive = GoogleDrive(gauth)
+file1 = drive.CreateFile({'title': 'main.py'})
+file1.SetContentString('hello')
+file1.Upload() # Files.insert()
+file1.InsertPermission({"role": "reader","type": "anyone"})
+print(file1['alternateLink'])
